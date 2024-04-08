4fd81de80074dcb1e43e120c518828979dea2dae
Nathan Fitzpatrick <45206408+NathanCQC@users.noreply.github.com>
2024-04-08 16:21:39 +0100

    test changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,7 @@
 """Tests for grid1q.main module."""
 
-from grid1q.main import hello_world
-
 
 def test_hello_world():
     """Test the hello_world function."""
-    assert hello_world() == "Hello, World!"
+    a = "Hello, World!"
+    assert a == "Hello, World!"
